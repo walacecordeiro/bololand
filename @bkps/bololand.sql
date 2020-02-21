3e1459f07e759d0216551d5ee04769577f3ce713
Lab 111 <lab.111@senacrjedu.local>
2020-02-21 13:35:43 -0300

    banco de dados

--- @bkps/bololand.sql
+++ @bkps/bololand.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 17-Fev-2020 às 12:24
--- Versão do servidor: 10.4.11-MariaDB
--- versão do PHP: 7.2.27
+-- Tempo de geração: 21-Fev-2020 às 17:34
+-- Versão do servidor: 10.4.10-MariaDB
+-- versão do PHP: 7.1.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -36,6 +36,15 @@ CREATE TABLE `endereco` (
   `uf` varchar(2) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Extraindo dados da tabela `endereco`
+--
+
+INSERT INTO `endereco` (`cep`, `logradouro`, `bairro`, `cidade`, `uf`) VALUES
+('21230043', 'Avenida Brasil', 'Irajá', 'Rio de Janeiro', 'RJ'),
+('23520120', 'Rua Antenor', 'Santa Cruz', 'Rio de Janeiro', 'RJ'),
+('23520220', 'Rua Justino de Assis', 'Santa Cruz', 'Rio de Janeiro', 'RJ');
+
 -- --------------------------------------------------------
 
 --
@@ -51,6 +60,32 @@ CREATE TABLE `produto` (
   `ativo` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Extraindo dados da tabela `produto`
+--
+
+INSERT INTO `produto` (`id_produto`, `nome`, `descricao`, `quant`, `valor`, `ativo`) VALUES
+(1, 'Mala de viagem', 'Mala de viagem grande com cores berrante', 334, '233.00', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tipo`
+--
+
+CREATE TABLE `tipo` (
+  `id_tipo` int(11) NOT NULL,
+  `tipo` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `tipo`
+--
+
+INSERT INTO `tipo` (`id_tipo`, `tipo`) VALUES
+(1, 'admin'),
+(2, 'cliente');
+
 -- --------------------------------------------------------
 
 --
@@ -65,8 +100,20 @@ CREATE TABLE `usuario` (
   `senha` varchar(100) NOT NULL,
   `cep` varchar(9) DEFAULT NULL,
   `numero` varchar(10) DEFAULT NULL,
-  `complemento` varchar(100) DEFAULT NULL
+  `complemento` varchar(100) DEFAULT NULL,
+  `tipo` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `usuario`
+--
+
+INSERT INTO `usuario` (`id_user`, `nome`, `email`, `tel`, `senha`, `cep`, `numero`, `complemento`, `tipo`) VALUES
+(1, 'Ana Silva', 'anasilva@email.com', '555-55555', '1111111111', '21230043', '34', '', NULL),
+(2, 'Maria das Silva', 'maria@email.com', '5555-55555', '40db719c907ad1017df5ea2888ad9ddf', '23520120', '32', '', NULL),
+(3, 'Maria do Bairro', 'mb@email.com', '15245201215', '989fdec2818448c05ead9b6d557277f7', '23520220', '23', '', 2),
+(4, 'Maria do Bairro', 'mb@email.com', '15245201215', '989fdec2818448c05ead9b6d557277f7', '23520220', '23', '', 2),
+(5, 'admin', 'admin@email.com', '5555-55555', 'af3e905ef7f5fd1f29100fe55858826c', '21230043', '55', '', 1);
 
 --
 -- Índices para tabelas despejadas
@@ -85,11 +132,18 @@ ALTER TABLE `produto`
   ADD PRIMARY KEY (`id_produto`);
 
 --
+-- Índices para tabela `tipo`
+--
+ALTER TABLE `tipo`
+  ADD PRIMARY KEY (`id_tipo`);
+
+--
 -- Índices para tabela `usuario`
 --
 ALTER TABLE `usuario`
   ADD PRIMARY KEY (`id_user`),
-  ADD KEY `cep` (`cep`);
+  ADD KEY `cep` (`cep`),
+  ADD KEY `tipo` (`tipo`);
 
 --
 -- AUTO_INCREMENT de tabelas despejadas
@@ -99,13 +153,19 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de tabela `produto`
 --
 ALTER TABLE `produto`
-  MODIFY `id_produto` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_produto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de tabela `tipo`
+--
+ALTER TABLE `tipo`
+  MODIFY `id_tipo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de tabela `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Restrições para despejos de tabelas
@@ -115,7 +175,8 @@ ALTER TABLE `usuario`
 -- Limitadores para a tabela `usuario`
 --
 ALTER TABLE `usuario`
-  ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`cep`) REFERENCES `endereco` (`cep`);
+  ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`cep`) REFERENCES `endereco` (`cep`),
+  ADD CONSTRAINT `usuario_ibfk_2` FOREIGN KEY (`tipo`) REFERENCES `tipo` (`id_tipo`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
